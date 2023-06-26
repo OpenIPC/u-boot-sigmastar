6769b129033729a6a77e292cda1f0c9b3585a4cc
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-06-26 20:39:36 +0200

    Update build script and fix warnings

--- build.sh
+++ build.sh
@@ -1,61 +1,49 @@
 #!/bin/sh
-
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabi-
+mkdir -p output
 
-mkdir output 2>&1 > /dev/null
-
-for soc in ssc325;do
-
+# spinor infinity6
+for soc in ssc325; do
 make distclean
 make infinity6_defconfig
 make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-
-./make_boot.sh 6
+./make_boot_spinor.sh i6
 mv BOOT.bin output/u-boot-${soc}-universal.bin
-
 done
 
-for soc in ssc335 ssc337 ssc335de ssc337de;do
-
+# spinor infinity6b0
+for soc in ssc335 ssc337 ssc335de ssc337de; do
 make distclean
 make infinity6b0_defconfig
 make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-
-./make_boot.sh 6b0
+./make_boot_spinor.sh i6b0
 mv BOOT.bin output/u-boot-${soc}-universal.bin
-
 done
 
-for soc in ssc30kq;do
-
+# spinor infinity6e
+for soc in ssc30kq ssc338q; do
 make distclean
 make infinity6e_defconfig
 make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-
-./make_boot.sh 6e0
+./make_boot_spinor.sh i6e
 mv BOOT.bin output/u-boot-${soc}-universal.bin
-
 done
 
-for soc in ssc338q;do
-
+# spinand infinity6e
+for soc in ssc338q; do
 make distclean
 make infinity6e_spinand_defconfig
 make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-
 ./make_boot_spinand.sh
-mv BOOT.bin output/u-boot-${soc}-nand.bin
-
+mv BOOT.bin output/u-boot-${soc}-spinand.bin
 done
 
-for soc in ssc338q;do
-
+# initramfs infinity6e
+for soc in ssc338q; do
 make distclean
 make infinity6e_spinand_defconfig
 sed -i "s/CONFIG_MS_SAVE_ENV_IN_NAND_FLASH=y/CONFIG_MS_SAVE_ENV_IN_NAND_FLASH=n/g" .config
 make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-
-cp u-boot_spinand.xz.img.bin output/u-boot-${soc}-ramfs.bin
-
+cp u-boot_spinand.xz.img.bin output/u-boot-${soc}-initramfs.bin
 done

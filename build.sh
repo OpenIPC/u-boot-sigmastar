a4226497d83caef26f650964f0c3539cef506d8c
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-11-23 12:57:34 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -5,67 +5,62 @@ export CROSS_COMPILE=arm-linux-gnueabi-
 rm -rf output
 mkdir -p output
 
+spinor() {
+	make $2_defconfig
+	make -j8 KCFLAGS=-DPRODUCT_NAME=$1
+	sh make_boot_spinor.sh $2
+	mv BOOT.bin output/u-boot-$1-nor.bin
+	make distclean
+}
+
+spinand() {
+	make $2_spinand_defconfig
+	make -j8 KCFLAGS=-DPRODUCT_NAME=$1
+	sh make_boot_spinand.sh $2
+	mv BOOT.bin output/u-boot-$1-nand.bin
+	make distclean
+}
+
 # spinor infinity6
 for soc in ssc325; do
-make distclean
-make infinity6_defconfig
-make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-./make_boot_spinor.sh i6
-mv BOOT.bin output/u-boot-${soc}-nor.bin
+	spinor $soc infinity6
 done
 
 # spinand infinity6
 for soc in ssc325de; do
-make distclean
-make infinity6_spinand_defconfig
-make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-./make_boot_spinand.sh i6
-mv BOOT.bin output/u-boot-${soc}-nand.bin
+	spinand $soc infinity6
 done
 
 # spinor infinity6b0
 for soc in ssc333 ssc335 ssc337 ssc335de ssc337de; do
-make distclean
-make infinity6b0_defconfig
-make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-./make_boot_spinor.sh i6b0
-mv BOOT.bin output/u-boot-${soc}-nor.bin
+	spinor $soc infinity6b0
+done
+
+# spinand infinity6b0
+for soc in ssc337de; do
+	spinand $soc infinity6b0
 done
 
 # spinor infinity6c
 for soc in ssc377; do
-make distclean
-make infinity6c_defconfig
-make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-./make_boot_spinor.sh i6c
-mv BOOT.bin output/u-boot-${soc}-nor.bin
+	spinor $soc infinity6c
 done
 
 # spinor infinity6e
 for soc in ssc30kd ssc30kq ssc338q; do
-make distclean
-make infinity6e_defconfig
-make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-./make_boot_spinor.sh i6e
-mv BOOT.bin output/u-boot-${soc}-nor.bin
+	spinor $soc infinity6e
 done
 
 # spinand infinity6e
 for soc in ssc338q; do
-make distclean
-make infinity6e_spinand_defconfig
-make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-./make_boot_spinand.sh i6e
-mv BOOT.bin output/u-boot-${soc}-nand.bin
+	spinand $soc infinity6e
 done
 
 # initramfs infinity6e
 for soc in ssc338q; do
-make distclean
-make infinity6e_spinand_defconfig
-sed -i "s/CONFIG_MS_SAVE_ENV_IN_NAND_FLASH=y/CONFIG_MS_SAVE_ENV_IN_NAND_FLASH=n/g" .config
-make -j5 KCFLAGS=-DPRODUCT_NAME=${soc}
-cp u-boot_spinand.xz.img.bin output/u-boot-${soc}-ram.bin
+	make infinity6e_spinand_defconfig
+	sed -i "s/CONFIG_MS_SAVE_ENV_IN_NAND_FLASH=y/CONFIG_MS_SAVE_ENV_IN_NAND_FLASH=n/g" .config
+	make -j8 KCFLAGS=-DPRODUCT_NAME=$soc
+	cp u-boot_spinand.xz.img.bin output/u-boot-$soc-ram.bin
+	make distclean
 done
-
-make distclean
--- make_boot_spinor.sh
+++ make_boot_spinor.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-IPL_DIR=ipl/${1:-i6b0}
+IPL=ipl/$1
 
 dd if=/dev/zero bs=1k count=128 | tr '\000' '\377' > BOOT.bin
 
-if [ "$1" = "i6c" ]; then
-	dd if=${IPL_DIR}/IPL.bin of=BOOT.bin bs=1k seek=0 conv=notrunc status=none
-	dd if=${IPL_DIR}/IPL_CUST.bin of=BOOT.bin bs=1k seek=60 conv=notrunc status=none
+if [ "$1" = "infinity6c" ]; then
+	dd if=$IPL/IPL.bin of=BOOT.bin bs=1k seek=0 conv=notrunc status=none
+	dd if=$IPL/IPL_CUST.bin of=BOOT.bin bs=1k seek=60 conv=notrunc status=none
 else
-	dd if=${IPL_DIR}/IPL.bin of=BOOT.bin bs=1k seek=0 conv=notrunc status=none
-	dd if=${IPL_DIR}/MXP_SF.bin of=BOOT.bin bs=1k seek=60 conv=notrunc status=none
-	dd if=${IPL_DIR}/IPL_CUST.bin of=BOOT.bin bs=1k seek=64 conv=notrunc status=none
+	dd if=$IPL/IPL.bin of=BOOT.bin bs=1k seek=0 conv=notrunc status=none
+	dd if=$IPL/MXP_SF.bin of=BOOT.bin bs=1k seek=60 conv=notrunc status=none
+	dd if=$IPL/IPL_CUST.bin of=BOOT.bin bs=1k seek=64 conv=notrunc status=none
 fi
 
 dd if=u-boot.xz.img.bin of=BOOT.bin bs=1k seek=128 conv=notrunc status=none
